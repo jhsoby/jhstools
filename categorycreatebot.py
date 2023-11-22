e91de1414624fd695187c7eab588aa48ec5949bf
Jon Harald Søby <jhsoby@gmail.com>
2023-11-22 18:55:46 +0100

    Add categorycreatebot
    Add bot to create monthly maintenance categories for the Norwegian
    Bokmål Wikipedia.

---
+++ categorycreatebot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+r"""
+This bot creates new monthly maintenance categories for
+categories that have a monthly setup in the Norwegian
+Bokmål Wikipedia.
+"""
+import pywikibot
+from pywikibot.page import Page
+from datetime import date, timedelta
+
+categories = [
+	[ "Stubber {}", "{{skjult kategori}}\n\n[[Kategori:Stubber etter tid]]" ],
+	[ "Artikler som trenger flere eller bedre referanser {}", "{{skjult kategori}}\n\n[[Kategori:Artikler som trenger flere referanser]]" ],
+	[ "Opprydning {}", "{{skjult kategori}}\n\n[[Kategori:Opprydning]]" ],
+	[ "Språkvask {}", "{{skjult kategori}}\n\n[[Kategori:Språkvask]]" ]
+]
+
+today = date.today()
+todaysmonth = today.strftime("%Y-%m")
+tomorrow = date.today() + timedelta(days=50)
+tomorrowsmonth = tomorrow.strftime("%Y-%m")
+
+def main(*args: str) -> None:
+	local_args = pywikibot.handle_args(args)
+	site = pywikibot.Site("wikipedia:no")
+	site.throttle.setDelays(delay=0.1, writedelay=0.1)
+	for cat in categories:
+		catname = "Kategori:" + cat[0].format(tomorrowsmonth)
+		catcontent = cat[1]
+		page = Page(site,catname)
+		if not page.exists():
+			page.text = catcontent
+			page.save("bot: Oppretter månedlig vedlikeholdskategori")
+
+if todaysmonth != tomorrowsmonth:
+	main()
